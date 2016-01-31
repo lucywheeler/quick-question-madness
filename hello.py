5dd06c8f9e6f1237527d1a85ae6bfa9c9c436ab4
Willie Wheeler <willie.wheeler@gmail.com>
2016-01-31 11:12:23 -0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+print "Hello. What is your name?"
+name = raw_input()
+
+print "Hello, " + name + ". It's nice to meet you."
+print " my name is Alexa I am a girl "
+
+print "What is your brother's name, " + name + "?"
+brother_name = raw_input()
+
+print "your brother's name is " +  brother_name + "."
+
+print "What is your favorite food?"
+favorite_food = raw_input()
+
+print  "your favorite food is " + favorite_food + "."
+
+if favorite_food.lower() == "dumplings":
+    print "that is mine too"
+else:
+    print "That is awesome!"
+
+print "What is your favorite color?"
+color = raw_input()
+
+print "my favorite color is purple"
+
+print "what is your favorite sport"
+sport = raw_input()
+
+print "that is very cool"
+
+if sport.lower() == "soccer":
+    print "that is mine too"
+else:
+    print "oh mine is soccer"
+
+print "what is your favorite kind of pet?"
+pet = raw_input()
+if pet.lower() == "dog":
+    print "that is mine"
+else:
+    print "mine is a dog"
